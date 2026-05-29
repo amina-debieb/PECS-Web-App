9c0bf56b34a7f66390f2229d916c9c9f550d2211
amina-debieb <aminadebieb704@gmail.com>
2026-05-29 06:18:21 -0700

    pecs_db.sql

--- pecs_db.sql
+++ pecs_db.sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 28, 2026 at 03:55 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `pecs_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
@@ -34,9 +13,7 @@ CREATE TABLE `categories` (
   `icon_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `categories`
---
+
 
 INSERT INTO `categories` (`id`, `name`, `color`, `icon_path`) VALUES
 (1, 'Nourriture', '#4CAF50', 'images/Nourriture.png'),
@@ -44,11 +21,7 @@ INSERT INTO `categories` (`id`, `name`, `color`, `icon_path`) VALUES
 (3, 'Actions', '#FF9800', NULL),
 (4, 'Sentiments', '#9C27B0', NULL);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pictograms`
---
 
 CREATE TABLE `pictograms` (
   `id` int(11) NOT NULL,
@@ -57,9 +30,7 @@ CREATE TABLE `pictograms` (
   `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `pictograms`
---
+
 
 INSERT INTO `pictograms` (`id`, `label`, `image_path`, `category_id`) VALUES
 (1, 'Banane', 'images/Banane.png', 1),
@@ -78,51 +49,26 @@ INSERT INTO `pictograms` (`id`, `label`, `image_path`, `category_id`) VALUES
 (14, 'Rit', 'images/Rit.jpg', 4),
 (15, 'Triste', 'images/Triste.jpg.png', 4);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
 
---
--- Indexes for table `pictograms`
---
+
 ALTER TABLE `pictograms`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `categories`
---
 ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `pictograms`
---
+
 ALTER TABLE `pictograms`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `pictograms`
---
 ALTER TABLE `pictograms`
   ADD CONSTRAINT `pictograms_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
